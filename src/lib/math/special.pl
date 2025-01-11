8c28237d32681e5ba44e8f197aa0f96832b01655
David C. Norris <david@precisionmethods.guru>
2025-01-11 15:39:59 -0500

    More traditional or clearer naming of Gamma predicates

--- src/lib/math/special.pl
+++ src/lib/math/special.pl
@@ -9,10 +9,10 @@ The underlying Rust implementations come from the puruspe crate.
               ,inverf/2
               ,inverfc/2
               ,gamma/2
-              ,gammp/3
-              ,gammq/3
+              ,gamma/3
+              ,gamma_P_Q/4
               ,invgammp/3
-              ,ln_gamma/2
+              ,log_gamma/2
                   %,beta/3
                   %,betai/4
                   %,invbetai/4
@@ -53,53 +53,51 @@ inverfc(ErfcX, X) :-
 
 %% gamma(+X, -Gamma)
 %
-% Gamma is Γ(X).
+% Gamma is Γ(X), the ordinary gamma function.
 gamma(X, Gamma) :-
     builtins:must_be_number(X, gamma/2),
     '$gamma'(X, Gamma).
 
-%% gammp(+A, +X, -P)
+%% gamma(+A, +X, -Gamma)
 %
-% P is γ(A,X)/Γ(A), the regularized _lower_ incomplete gamma function.
-% X ≥ 0 is the upper limit of integration
+% Gamma is Γ(A,X), the _upper_ incomplete gamma function.
 % A > 0 is the shape parameter
-gammp(A, X, P) :-
-    builtins:must_be_number(A, gammp/3),
-    builtins:must_be_number(X, gammp/3),
-    '$gammp'(A, X, P).
-
-%% gammq(+A, +X, -Q)
-%
-% Q is Γ(A,X)/Γ(A), the regularized _upper_ incomplete gamma function.
 % X ≥ 0 is the lower limit of integration
-% A > 0 is the shape parameter
-gammq(A, X, Q) :-
+gamma(A, X, Gamma) :-
     builtins:must_be_number(A, gammq/3),
     builtins:must_be_number(X, gammq/3),
+    '$gammq'(A, X, Q),
+    gamma(A, GammaA),
+    Gamma is Q*GammaA.
+
+%% gamma_P_Q(+A, +X, -P, -Q)
+%
+% P is γ(A,X)/Γ(X), the regularized _lower_ incomplete gamma function.
+% Q is Γ(A,X)/Γ(X), the regularized _upper_ incomplete gamma function.
+% A > 0 is the shape parameter
+% X ≥ 0 is the lower limit of integration
+gamma_P_Q(A, X, P, Q) :-
+    builtins:must_be_number(A, gamma_P_Q/4),
+    builtins:must_be_number(X, gamma_P_Q/4),
+    '$gammp'(A, X, P),
     '$gammq'(A, X, Q).
 
-%% invgammp(+P, +A, -X)
+%% invgammp(+A, +P, -X)
 %
 % X is the unique solution of P = P(A,X), where P(-,-) is the
 % regularized lower incomplete gamma function.
 % P ∈ [0,1] is a probability
 % A > 0 is the _shape parameter_
-invgammp(P, A, -X) :-
-    builtins:must_be_number(P, invgammp/3),
+invgammp(A, P, X) :-
     builtins:must_be_number(A, invgammp/3),
+    builtins:must_be_number(P, invgammp/3),
     '$invgammp'(P, A, X).
 
-/* TODO: Investigate this..  Is there some issue of principal
-         values to consider?
-?- A = 3, X = 1.2, gammp(A, X, P), invgammp(P, A, Xb).
-   A = 3, X = 1.2, P = 0.12051290121804392, Xb = - (1.2).
-*/
-
-%% ln_gamma(+X, -LnGamma)
+%% log_gamma(+X, -LogGamma)
 %
-% LnGamma is ln(Γ(X)), the natural logarithm of Γ(X).
-ln_gamma(X, LnGamma) :-
-    builtins:must_be_number(X, ln_gamma/2),
+% LogGamma is ln(Γ(X)), the natural logarithm of Γ(X).
+log_gamma(X, LnGamma) :-
+    builtins:must_be_number(X, log_gamma/2),
     '$ln_gamma'(X, LnGamma).
 
 %% TODO: beta(+Z, +W, -B)
